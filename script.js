5baf8453234aa2adc008a2a5ad66194fef4a386d
CameronJones5 <Cameron.jones@hgs.hiddengeniusproject.org>
2022-02-12 19:51:13 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+var myname = "Cameron";
+var mybros = "Sione & Rafael";
+var mymessage = "we will get better at leanring how work our projects";
+console.log("My name is " + myname + ", I am on a team with " + mybros + ". I want my bros to know that" + 
+mymessage + ".");
